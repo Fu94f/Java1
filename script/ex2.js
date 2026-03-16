97403e360b2edb0b0a45ae67c2ec7655f07c46b0
Fu94f <fulvia.franco1994@gmail.com>
2026-03-16 13:28:57 +0100

    ex2 and ex3

---
+++ script/ex2.js
@@ -0,0 +1,25 @@
+// EXERCISE 2
+
+
+let firstName= "Foo";
+let age= 31;
+let favouriteColour= "purple";
+
+//console.log(firstName);
+console.log(`my name is ${firstName}`);
+
+//console.log( typeof firstName);
+
+//console.log(age);
+console.log(`my age is ${age}`);
+
+//console.log(typeof age);
+
+
+//console.log(favouriteColour);
+console.log(`my favourite colour is ${favouriteColour}`);
+
+//console.log(typeof favouriteColour);
+
+
+console.log(`my name is ${firstName}, I am ${age} years old and my favourite colour is ${favouriteColour}`);
---
+++ script/ex3.js
@@ -0,0 +1,30 @@
+// EXERCISE 3
+
+let dogs=40;
+
+//dogs = 40 + 1;
+//dogs = dogs + 1;
+//dogs = dogs - 1;
+//dogs = dogs * 2;
+//dogs = dogs / 2;
+//dogs = dogs % 2;
+//extradog = dogs % 2;
+
+let cats= 30;
+ 
+
+//cats = cats + 2;
+//cats = cats - 2;
+//cats = cats * 2;
+//cats = cats / 2;
+//cats = cats % 2;
+//extracats = cats % 2;
+
+let animals = cats + dogs ;
+
+
+console.log(dogs);
+console.log(cats);
+
+console.log(animals);
+console.log(animals - dogs);
